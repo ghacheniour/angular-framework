35854032376fdf7835afca44b717aa4896dc5722
ghassen <ghassen@alatest.com>
2018-01-19 11:09:38 +0100

    add initial commit for controllers

--- src/angular_public.js
+++ src/angular_public.js
@@ -15,6 +15,7 @@ function publishExternalAPI() {
     ngModule.provider('$httpParamSerializer', require('./http').$HttpParamSerializerProvider);
     ngModule.provider('$httpParamSerializerJQLike', require('./http').$HttpParamSerializerJQLikeProvider);
     ngModule.provider('$compile', require('./compile'));
+    ngModule.provider('$controller', require('./controller'));
 }
 
 module.exports = publishExternalAPI;
